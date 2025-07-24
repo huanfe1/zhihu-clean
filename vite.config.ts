3170116584f73991e08e9516c2e80429a782cf21
huanfe1 <41602338+huanfe1@users.noreply.github.com>
2025-07-24 20:32:31 +0800

    feat: don't minify dist file

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
+import { URL, fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import monkey from 'vite-plugin-monkey';
-import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,13 +13,13 @@ export default defineConfig({
                 namespace: 'https://huanfei.top/',
                 author: 'huanfei',
                 match: ['*://*.zhihu.com/*'],
-                "run-at": 'document-start',
+                'run-at': 'document-start',
                 description: '将网页主体部分变宽，去除杂冗部分，并添加一些实用的功能',
                 license: 'MIT',
             },
         }),
     ],
-    build: { minify: true },
+    // build: { minify: true },
     resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
     esbuild: { pure: ['console.log'] },
     css: { preprocessorOptions: { scss: { api: 'modern' } } },
